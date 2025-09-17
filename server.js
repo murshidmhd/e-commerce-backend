cd3e0c6cc1a9482765b26b5ca09059756c3887b2
murshidmhd <murshidmuhammad65@gmail.com>
2025-09-17 15:41:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 10000;
 
+// Enable CORS
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 server.use(middlewares);
 server.use(router);
+
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
